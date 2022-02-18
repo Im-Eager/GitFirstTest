fd35b7ab1c7709fc6298588de7a51cf05e341ec9
Im-Eager <83980495+Im-Eager@users.noreply.github.com>
2022-02-18 13:58:42 +0000

    This is my first change

--- GitFirstTest/src/academy/mindswap/Main.java
+++ GitFirstTest/src/academy/mindswap/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("My first commit and push");
+        System.out.println("My first change");
     }
 }
