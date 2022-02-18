9ea2f171857acac2ce278ea4a736cceaa03d0b56
Im-Eager <83980495+Im-Eager@users.noreply.github.com>
2022-02-18 13:55:12 +0000

    This is my first Git commit and push

---
+++ GitFirstTest/src/academy/mindswap/Main.java
@@ -0,0 +1,9 @@
+package academy.mindswap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("My first commit and push");
+    }
+}
