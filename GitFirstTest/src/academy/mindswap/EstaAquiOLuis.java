761d208d0b5d3600a40988b3af5a0ca277b4fbeb
Luís Faria <ptluisfaria@gmail.com>
2022-02-18 15:55:01 +0000

    feios do cacete

--- GitFirstTest/src/academy/mindswap/EstaAquiOLuis.java
+++ GitFirstTest/src/academy/mindswap/EstaAquiOLuis.java
@@ -4,5 +4,7 @@ public class EstaAquiOLuis {
 
     public static void main(String[] args) {
         System.out.println("o mais bonito! De todos!");
+        System.out.println(" e o João é o mais feio!");
+        System.out.println("O Tiago é o dono desta merda toda!");
     }
 }
