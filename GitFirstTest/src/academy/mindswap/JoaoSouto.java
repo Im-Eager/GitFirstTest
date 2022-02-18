7da6a43ba95b316a8b00c863d3e54a955a9dc95f
João Souto <jpsoutost@gmail.com>
2022-02-18 14:50:28 +0000

    merge

--- GitFirstTest/src/academy/mindswap/JoaoSouto.java
+++ GitFirstTest/src/academy/mindswap/JoaoSouto.java
@@ -5,5 +5,6 @@ public class JoaoSouto {
         System.out.println("Este gajo é um campeão.");
         System.out.println("Isto é só mais um teste deste campeão");
         System.out.println("Isto é só mais um teste deste campeão");
+
     }
 }
