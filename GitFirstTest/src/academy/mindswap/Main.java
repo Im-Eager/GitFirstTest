4da57e6022dfd0600f99e571f244923d7b0118be
Tiago Correia <correiatiago1987@gmail.com>
2022-02-18 14:13:28 +0000

    Added TiagoCorreia.java and line 10 on Main

--- GitFirstTest/src/academy/mindswap/Main.java
+++ GitFirstTest/src/academy/mindswap/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("My first commit and push");
-        System.out.println("My first change");
-        System.out.println("And this my second");
-        System.out.println("Tiago Correia");
+
     }
 }
