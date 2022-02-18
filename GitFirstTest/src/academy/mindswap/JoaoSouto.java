6e6e764f02f30588227bafde1a64050ee3d6ce15
João Souto <jpsoutost@gmail.com>
2022-02-18 14:44:08 +0000

    testing branches

--- GitFirstTest/src/academy/mindswap/JoaoSouto.java
+++ GitFirstTest/src/academy/mindswap/JoaoSouto.java
@@ -4,5 +4,6 @@ public class JoaoSouto {
     public static void main(String[] args) {
         System.out.println("Este gajo é um campeão.");
         System.out.println("Isto é só mais um teste deste campeão");
+        System.out.println("Isto é só mais um teste deste campeão");
     }
 }
